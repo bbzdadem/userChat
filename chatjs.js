bb8a4d2ba6c06c7df6f075d4affc1fb43b0c855c
Adam Baba <bbzdadem@gmail.com>
2021-02-23 14:38:55 +0400

    third

--- chatjs.js
+++ chatjs.js
@@ -1,42 +1,51 @@
-var phoneNumber = $('#customer-input');
-var readyButton = $('#customer-ready-button');
-var customerState = $('#customer-state');
-var customerMessageInput = $('#customer-message-input');
-var customerSendButton = $('#customer-send-button');
-var customerMessages = $('#customer-messages');
-var token;
-var channelSid;
-var device;
-var myChannel;​
-readyButton.click(function() {
-    $.post('https://chat.bankoff.org/api/token/generate?identity=' + phoneNumber.val())
-        .done(function(data) {
-            token = data.token;
-            channelSid = data.channelSid;
-            Twilio.Chat.Client.create(token).then(function(client) {
-                client.getChannelBySid(channelSid).then(function(channel) {
-                    myChannel = channel;
-                    myChannel.join();
-                    // myChannel.getMessages().then(function(messages) {
-                    //     let length = messages.items.length;
-                    //     for (i = 0; i < length; i++) {
-                    //         let message = messages.items[i];
-                    //         customerMessages.append(message.author + ' : ' + message.body + "<br>");
-                    //     }
-                    // })
-                    customerMessageInput.prop('disabled', false);
-                    customerSendButton.prop('disabled', false);
-                    myChannel.on('messageAdded', function(message) {
-                        customerMessages.append(message.author + ' : ' + message.body + "<br>");
+        var readyButton = document.getElementById("ready-button")
+        var customerState = document.getElementById("customer-state")
+        var customerMessageInput = document.getElementById("message-input")
+        var customerSendButton = document.getElementById("message-send")
+        var customerMessages = document.getElementById("messagebox")
+        var token;
+        var channelSid;
+        var myChannel;
+        var getChat;
+
+        // function getChat() {
+        //     console.log("worked")
+        //     const URL = 'https://chat.bankoff.org/api/token/generate?identity=';
+        //     const FULL_URL = `${URL}'994552825540'`
+
+        //     const ChatPromise = fetch(FULL_URL);
+        //     return ChatPromise.then((response) => {
+        //         return response.json();
+        //     })
+
+        // }
+
+        // getChat();
+
+        function readyButton() {
+
+            fetch('https://chat.bankoff.org/api/token/generate?identity=994552825540', {
+                method: 'POST'
+            }).then(function(data) {
+                data.json();
+                token = data.token;
+                channelSid = data.channelSid;
+                Twilio.Chat.Client.create(token).then(function(client) {
+                    client.getChannelBySid(channelSid).then(function(channel) {
+                        myChannel = channel;
+                        myChannel.join();
+                        customerMessageInput.prop('disabled', false);
+                        customerSendButton.prop('disabled', false);
+                        myChannel.on('messageAdded', function(message) {
+                            customerMessages.append(message.author + ' : ' + message.body + "<br>");
+                        })
                     })
-                })
-            });
-        })
-        .fail(function(error) {
-            customerState.val('Server not responding...');
-        });
-});​
-customerSendButton.click(function() {
-    myChannel.sendMessage(customerMessageInput.val())
-    customerMessageInput.val("");
-})
+                });
+            })
+        };
+
+
+        function SendButton() {
+            myChannel.sendMessage(customerMessageInput.val())
+            customerMessageInput.val("");
+        };
